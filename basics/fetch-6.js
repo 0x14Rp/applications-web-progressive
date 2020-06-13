338a8945cc51f37c5ef2294e4c5908df1e338fc8
Rafael <elgondu@gmail.com>
2020-06-13 15:14:43 -0400

    fecht y promesas

---
+++ basics/fetch-6.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+
+fetch('no-encontrado.html')
+    .then( resp => resp.text() )
+    .then( html => {
+
+        let body = document.querySelector('body');
+        body.innerHTML = html;
+
+
+    })
+    .catch( error => {
+        console.log('Error en la petición');
+        console.log(error);
+    });
+
+
+
+
+
+
---
+++ basics/test.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+
+
+function f(numero) {
+return numero + 1;
+}
+
+
+console.log(f(5));
